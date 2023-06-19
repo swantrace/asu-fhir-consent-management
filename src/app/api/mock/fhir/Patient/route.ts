8995f5aa4d4b4d387561233474574145b0965d03
swantrace <frederickhong.5@gmail.com>
2023-06-19 13:10:06 -0700

    create mock apis

--- src/app/api/mock/fhir/Patient/route.ts
+++ src/app/api/mock/fhir/Patient/route.ts
@@ -1,5 +1,24 @@
 import { NextResponse } from 'next/server';
+import patients from '../../../../../lib/mock/Patient';
+import questionnaireResponses from '../../../../../lib/mock/QuestionnaireResponse';
+
 export function GET(request: Request) {
-  console.log('request: ', request);
-  return NextResponse.json({ hello: 'world' });
+  const patient = patients.find((patient) =>
+    patient.resource.identifier
+      .map((i) => i?.value ?? false)
+      .filter((i) => i)
+      .includes(`mailto:${new URL(request.url).searchParams.get('identifier')}`)
+  );
+  return patient
+    ? NextResponse.json(
+        {
+          ...patient,
+          questionnaireResponses: questionnaireResponses.filter(
+            (qr) =>
+              qr.resource.subject.reference === `Patient/${patient.resource.id}`
+          )
+        },
+        { status: 200 }
+      )
+    : NextResponse.json({ error: 'not found' }, { status: 404 });
 }
