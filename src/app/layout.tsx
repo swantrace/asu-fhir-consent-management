411f3f38c7552e356c5980e2f77daa384a009ecf
swantrace <frederickhong.5@gmail.com>
2023-06-20 00:32:37 -0700

    fix some issues of loading lform scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 /* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import './globals.css';
 import { Suspense } from 'react';
-import Script from 'next/script';
 import Nav from './nav';
-import { lformScripts, getRealUrl } from '../lib/lforms';
-
-const scripts = lformScripts.slice(0, -1);
 
 export const metadata = {
   title: 'ASU Fhir Consent Management System',
@@ -25,13 +21,6 @@ export default async function RootLayout({
           <Nav />
         </Suspense>
         {children}
-        {scripts.map((script) => (
-          <Script
-            src={getRealUrl(script)}
-            strategy="beforeInteractive"
-            key={script}
-          />
-        ))}
         <link
           rel="stylesheet"
           href="https://clinicaltables.nlm.nih.gov/lforms-versions/30.0.0-beta.6/webcomponent/styles.css"
--- src/lib/lforms/index.ts
+++ src/lib/lforms/index.ts
@@ -2,13 +2,13 @@ export const lformCdnBase =
   'https://clinicaltables.nlm.nih.gov/lforms-versions/30.0.0-beta.6/';
 
 export const lformScripts = [
-  'webcomponent/assets/lib/zone.min.js',
-  'webcomponent/scripts.js',
-  'webcomponent/runtime-es5.js',
-  'webcomponent/polyfills-es5.js',
-  'webcomponent/main-es5.js',
-  'fhir/lformsFHIRAll.min.js'
-];
+  ['webcomponent/assets/lib/zone.min.js', 'beforeInteractive'],
+  ['webcomponent/scripts.js', 'beforeInteractive'],
+  ['webcomponent/runtime-es5.js', 'beforeInteractive'],
+  ['webcomponent/polyfills-es5.js', 'beforeInteractive'],
+  ['webcomponent/main-es5.js', 'beforeInteractive'],
+  ['fhir/lformsFHIRAll.min.js', 'lazyOnload']
+] as const;
 
 export const getRealUrl = (url: string) => {
   if (process.env.NEXT_PUBLIC_USE_LFORMS_CDN) {
